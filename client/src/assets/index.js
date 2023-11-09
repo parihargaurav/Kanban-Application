bfeede02053941db873d839c6db8caf659b89446
Gaurav <parihargaurav527@gmail.com>
2023-11-10 01:00:19 +0530

    firsr commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  images: {
+    logoDark: require('./images/kanban.png'),
+    appIcon: require('./images/kb.png')
+  },
+  colors: {
+    secondary: '#292929'
+  }
+}
+
+export default assets
